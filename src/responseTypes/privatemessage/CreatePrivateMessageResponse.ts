965eaf472cec64f80b514ae2cf782d746a6307a3
Ategon <benjamin@barbeau.net>
2024-02-08 09:44:24 -0500

    Add correct import to createprivatemessage

--- src/responseTypes/privatemessage/CreatePrivateMessageResponse.ts
+++ src/responseTypes/privatemessage/CreatePrivateMessageResponse.ts
@@ -1,3 +1,5 @@
+import { PrivateMessageView } from '../../types/privatemessage/PrivateMessageView.js';
+
 export type CreatePrivateMessageResponse = {
   private_message_view: PrivateMessageView;
 };
